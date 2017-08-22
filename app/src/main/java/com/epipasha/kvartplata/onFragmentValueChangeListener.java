45e5471e1d4de6a8a585d1564ba71e70ef48b3be
Pavel <epipasha@gmail.com>
2017-08-22 18:58:57 +0300

    Add new classes for paymentFragment

---
+++ app/src/main/java/com/epipasha/kvartplata/onFragmentValueChangeListener.java
@@ -0,0 +1,11 @@
+package com.epipasha.kvartplata;
+
+/**
+ * Created by Pavel on 21.08.2017.
+ */
+
+public interface onFragmentValueChangeListener {
+
+    public void onFragmentValueChange();
+
+}
